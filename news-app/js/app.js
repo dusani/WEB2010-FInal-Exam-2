d4a7996adae91c37e435ea7a7b1d65736883edf9
Daniel Usani <daniel.e.usani@gmail.com>
2017-11-19 19:01:45 -0500

    WEB2010 Final Exam 2

--- news-app/js/app.js
+++ news-app/js/app.js
@@ -1,11 +1,12 @@
 const searchTerm = document.querySelector('#searchTerm')
 const searchbutton = document.querySelector('#searchbutton')
-const output = document.querySelector('.output')
+// const output = document.querySelector('.output')
+const accordion = document.querySelector('#accordion')
 
 
-const ajaxSearch = function () {
+const ajaxSearch = function() {
 
-  output.innerHTML = ""
+  accordion.innerHTML = ""
 
   const API_URL = `https://newsapi.org/v2/everything?q=${searchTerm.value}&apiKey=e51ffae2cc2e497c9b7e6fb79934ffff`
 
@@ -15,28 +16,46 @@ const ajaxSearch = function () {
     url: API_URL,
     async: true,
     dataType: "json",
-    success: function (data) {
+    success: function(data) {
       console.log(data.articles);
 
-      for(let i in data.articles){
-        output.innerHTML += `
-          <li class="listItem">
-            <div class="listItemDiv firstDiv">
-              <a href="${data.articles[i].url}" target="_blank"><img src="${data.articles[i].urlToImage}" alt=""></a>
+      for (let i in data.articles) {
+        accordion.innerHTML += `
+        <div class="card">
+          <div class="card-header" role="tab" id="heading">
+            <h5 class="mb-0"><a href="#collapse${[i]}" data-parent="#accordion" data-toggle="collapse">
+              ${data.articles[i].title}
+                  </a></h5>
+          </div>
+
+          <div id="collapse${[i]}" class="collapse">
+            <div class="d-flex">
+              <div class="align-self-center"><img src="${data.articles[i].urlToImage}"></div>
+              <div class="card-body col">
+                ${data.articles[i].description}
+                <a href="${data.articles[i].url}" target="_blank"><button class="btn btn-primary">Read more</button></a>
+
+                <div class="d-flex flex-row justify-content-between">
+                  <div class="">
+                    <b>Source:</b>
+                    <a href="http://${data.articles[i].source.name}" target="_blank">${data.articles[i].source.name}</a>
+                  </div>
+                  <div class="">
+                    <b>Author:</b> <span>${data.articles[i].author}</span>
+                  </div>
+                  <div class="">
+                    <b>Published At:</b> <span>${data.articles[i].publishedAt}</span>
+                  </div>
+                </div>
+              </div>
             </div>
-            <div class="listItemDiv secondDiv">
-              <a href="${data.articles[i].url}" target="_blank">${data.articles[i].title}</a>
-              <p>${data.articles[i].description} <a href="${data.articles[i].url}" target="_blank"><button>Read more</button></a></p>
-              <p><b>Source:</b> <a href="http://${data.articles[i].source.name}" target="_blank">${data.articles[i].source.name}</a>&emsp;
-                <b>Author:</b> <span>${data.articles[i].author}</span>&emsp;
-                <b>Published At:</b> <span>${data.articles[i].publishedAt}</span></p>
-            </div>
-          </li>
+          </div>
+        </div>
         `
       }
 
     },
-    error: function (error) {
+    error: function(error) {
       console.log("There was an error");
     }
 
